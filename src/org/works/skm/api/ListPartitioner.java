c972d3ef9a6d1a1326928f4d03bc448265ee1fd5
kmanter <kmantersedki@hotmail.fr>
2016-06-01 00:26:28 +0200

    improve the performance of partition function

--- src/org/works/skm/api/ListPartitioner.java
+++ src/org/works/skm/api/ListPartitioner.java
@@ -7,32 +7,33 @@ import org.works.skm.exception.SplitZeroSizeException;
 
 public class ListPartitioner {
 	
-	
 	/**
 	 * Split is a function that divide an Array to many subArray with a maximal size equal to @param length.
 	 *
 	 * @param inList the in list
-	 * @param length the length used to split
-	 * @return the list partitioned
+	 * @param len the length used to split
+	 * @return outList the list partitioned
 	 * @throws SplitZeroSizeException the split zero size exception
 	 */
-	@SuppressWarnings("rawtypes")
-	public static List<List> Split(List inList, int length) throws SplitZeroSizeException {
-		if(length == 0){
+	public static <T> List<List<T>> partition(List<T> inList, int len) throws SplitZeroSizeException{
+		if(len == 0){
 			throw new SplitZeroSizeException("split number should be >0");
 		}
-	    List<List> outlists = new ArrayList<>();
-	    List<Object> tmpList ;
-	    int i = 0;
-	    while (i < inList.size()) {
-	    	tmpList = new ArrayList<>();
-	    	for (int j = 0; i < inList.size() && j<length; j++) {
-				tmpList.add(inList.get(i));
-				i++;
+		List<List<T>> outlist = new ArrayList<>();
+		int pos=0;
+		int inListSize =inList.size();
+		boolean looping = true;
+		do{
+			if( (pos + len) < inListSize )
+				outlist.add(inList.subList(pos, pos+len));
+			else{
+				outlist.add(inList.subList(pos, inListSize));		
+				looping = false;
 			}
-	    	outlists.add(tmpList);
-	    }
-	    return outlists;
+			pos += len;
+		}while(looping);
+		
+		return outlist;
 	}
 	
 }
--- src/org/works/skm/tests/ListPartitionerTests.java
+++ src/org/works/skm/tests/ListPartitionerTests.java
@@ -20,9 +20,9 @@ public class ListPartitionerTests{
 	 */
 	@Test(expected=SplitZeroSizeException.class)
 	public void testSplit_List_Length_8_split_0() throws SplitZeroSizeException{
-		ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 0);
+		ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 0);
 	}	
-	
+
 	/**
 	 * Test split List (size=8) with 1.
 	 *
@@ -30,7 +30,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_1() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 1), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 1), 
 		           Arrays.asList(Arrays.asList("1"), Arrays.asList("2"), Arrays.asList("3"),
 		        		   		 Arrays.asList("4"), Arrays.asList("5"), Arrays.asList("6"),
 		        		   		 Arrays.asList("7"),Arrays.asList("8")));
@@ -44,7 +44,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_2() throws SplitZeroSizeException {
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 2), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 2), 
 		           Arrays.asList(Arrays.asList("1","2"), Arrays.asList("3","4"),
 		        		   		 Arrays.asList("5","6"), Arrays.asList("7","8")));
 	}
@@ -56,7 +56,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_3() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 3), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 3), 
 		           Arrays.asList(Arrays.asList("1","2","3"),Arrays.asList("4","5","6"),
 		        		   	 	 Arrays.asList("7","8")));
 	}
@@ -68,7 +68,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_4() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 4), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 4), 
 		           Arrays.asList(Arrays.asList("1","2","3","4"),
 		        		   	 	 Arrays.asList("5","6","7","8")));
 	}
@@ -80,7 +80,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_5() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 5), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 5), 
 		           Arrays.asList(Arrays.asList("1","2","3","4","5"),
 		        		   	 	 Arrays.asList("6","7","8")));
 	}
@@ -92,7 +92,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_8() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 8), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 8), 
 		           Arrays.asList(Arrays.asList("1","2","3","4","5","6","7","8")));
 	}
 
@@ -103,7 +103,7 @@ public class ListPartitionerTests{
 	 */
 	@Test
 	public void testSplit_List_Length_8_split_10() throws SplitZeroSizeException{
-		assertEquals(ListPartitioner.Split(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 10), 
+		assertEquals(ListPartitioner.partition(Arrays.asList("1", "2", "3", "4", "5","6","7","8"), 10), 
 		           Arrays.asList(Arrays.asList("1","2","3","4","5","6","7","8")));
 	}
 }
